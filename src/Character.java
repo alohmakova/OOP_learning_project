f770b22c59c63cea5aa24226bf5db34b7f2c537f
Anastasiya Lakhmakova <anastasiya.lakhmakova@gmail.com>
2024-09-05 18:19:55 +0200

    Step 2: Add More Attributes and Methods
    ● Goal: Enhance the Character class by adding more functionality.
    1. Add More Attributes:
    ○ Add attributes like defense, level, and experience.
    2. Add Methods:
    ○ Create a method takeDamage(int damage) that reduces the character’s
    health by the damage amount, considering the defense attribute.
    ○ Create a method attack() that returns the character’s strength as the
    damage value.
    3. Update the Testing:
    ○ Update the main method to create a scenario where one character attacks
    another, and then print out the stats to show the results.

--- src/Character.java
+++ src/Character.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
+
 public class Character {
-  //create Attributes
+  //Step 1: create Attributes
   //I put "private" modifier because I use getters and setters
   private String name;
   private double  health;
   private int strength;
+
+  //Step 2: new attributes
+  private String defense;
+  int damage;
+  int level;
+  double experience;
 
   //generate Getters and Setters to modify and view the attribute values
   public String getName() {
@@ -30,17 +38,70 @@ public class Character {
     this.strength = strength;
   }
 
-  //Create a constructor to initialize the Attributes
+  public String getDefense() {
+    return defense;
+  }
+
+  public void setDefense(String defense) {
+    this.defense = defense;
+  }
+
+  //Step 1: create a constructor to initialize the Attributes
     public Character(String name, double  health, int strength){
     this.setName(name);
     this.setHealth(health);
     this.setStrength(strength);
+    this.setDefense(" ");
 }
-  //Write a method that prints character's stats
+  //Step 1: method that prints character's stats
   public void displayStats(){
-    System.out.println("My name is: " + getName());
-    System.out.println("I have " + getHealth() + " points of healts");
-    System.out.println("My strength is: " + getStrength());
+    System.out.println("My name is: " + name);
+    System.out.println("I have " + health + " points of healts");
+    System.out.println("My strength is: " + strength);
+    System.out.println("My defense is: " + defense);
+    System.out.println("---------------------------");
+  }
+  //Step 2: method gives possibility to choose the defense
+  public void chooseDefense(){
+    Scanner scan = new Scanner(System.in);
+    System.out.println(getName() +", choose the armor to defend yourself: \n\t[1]helmet \n\t[2]breastplate \n\t[3]forearm protector \n\t[0]nothing");
+    int choiseOfDefense = scan.nextInt();
+    switch (choiseOfDefense){
+      case 1:
+        defense = "helmet";
+        System.out.println(name + " can defend yourself with the " + defense + " now");
+        break;
+      case 2:
+        defense = "breastplate";
+        System.out.println(name + " can defend yourself with the " + defense + " now");
+        break;
+      case 3:
+        defense = "forearm protector";
+        System.out.println(name + " can defend yourself with the " + defense + " now");
+        break;
+      default:
+        defense = " ";
+        System.out.println(name + " do not have any defense");
+    }
+  }
+
+  //Step 2: method that reduces the character's health by the damage amount, considering the defense attribute
+  public double takeDamage(int damage){
+    if (defense.equals(" ") || defense.isEmpty()) {//defense.isEmpty() there is ""
+      health = getHealth() - damage;
+    }else if (defense.equals("helmet")) {
+      health = getHealth() - damage/1.5;//40
+    }else if (defense.equals("breastplate")) {
+      health = getHealth() - damage /2.0;//30
+    }else if (defense.equals("forearm protector")) {
+      health = getHealth() - damage /1.2;
+    }
+    return health;
+  }
+  //Step 2: method that returns the character’s strength as the damage value
+  public int attack(){
+    damage = getStrength();
+    return damage;
   }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
 
-    //Create a Character object and initialize it
-    Character character1 = new Character("King", 10.0, 100);
+    //Step 1: create a Character object and initialize it
+    Character character1 = new Character("Knight", 100.0, 50);
+    Character character2 = new Character("King", 100.0, 60);
 
-    //call displayStats() method to show the output
+    //Step 1: call displayStats() method to show the output
     character1.displayStats();
+    character2.displayStats();
+    character1.chooseDefense();
+
+    //Step 2: create a scenario where one character attacks another
+    System.out.println(character1.getName() + " took damage as a result of the "+ character2.getName() + "'s attack " +
+                        " and now "+ character1.getName() +"'s health is equal to " +  character1.takeDamage(character2.attack()) + " points " +
+                        "\n--------------------------");
+    //Step 2: print out the stats to show the results
+    character1.displayStats();
+    character2.displayStats();
+
   }
 }
